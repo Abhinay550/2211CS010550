0a0029826af0fb407bc48f9acdaffacd4e1e7526
VALLAMKONDA ABHINAY <vallamkondaabhi@gmail.com>
2025-01-24 12:16:28 +0530

    pushing the files to repo

---
+++ loop.js
@@ -0,0 +1,62 @@
+//  console.log("GOod Morning!");
+  //! for loop
+ // for(let i=1;i<=5;i++){
+   // console.log("Good Morning");
+  //}
+
+  //let n = 100;
+  //for(let i = 0; i<=n;i++)
+  //{
+    //console.log(i);
+  //}
+
+  //for(let i=1; i <= 100;i++){
+    //if(i % 2 == 0){
+      //console.log(i,"is an even number")
+    //}else{
+      //console.log(i,"odd number")
+    //}
+
+  //}
+//let i=1;
+//while(i <= 5){
+  //console.log("Good Morning!!!");
+  //i++;
+//}
+
+//let i = 1;
+//let n=5;
+//do{
+  //i++;
+  //console.log("Batch 3")
+//}while(i<=n)
+ // ! function expression
+//let x = function(){
+  //console.log("function expression is executing");
+//};
+//();
+
+//! LIFE
+//(function(){
+//  console.log("IIFE");
+//})();
+// ! ARROW FUNCTION
+//function demo()
+//{
+  //console.log("Hello");
+//}
+//demo();
+
+//let y = ()=>console.log("Arrow function");
+//y();
+
+let z =_=>console.log("Arrow function");
+z();
+
+//let x =a=>console.log(a);
+//x(5);
+
+let x=_=>{console.log("hi");
+  console.log("Hello");
+  console.log("Bye");}
+  x();
